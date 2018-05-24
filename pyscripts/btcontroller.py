bf9939d6e80629c49978d2d98cd2f8807d570ecf
Amit Jha <amitnjha@gmail.com>
2018-05-24 16:52:05 -0500

    looping btcontroller

--- pyscripts/btcontroller.py
+++ pyscripts/btcontroller.py
@@ -3,28 +3,46 @@ import bluetooth
 from robotcontrol import *
 
 
+def serve_client():
+        server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        port = 1
+        server_sock.bind(("",port))
+        server_sock.listen(1)
+        client_sock,address = server_sock.accept()
+        print("Accepted from ",address)
+        while True:
+                try:
+                        data = client_sock.recv(1024)
+                        print data
+                        action=data.split()
+                        doAction(action[0],action[1]) 			
+                        if data == "CLOSE 0":
+                                break
+                except:
+                        print('Disconnected, listening again')
+                        server_sock.close()
+                        serve_client()
+
 print 'starting bt controller'
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-port = 1
-server_sock.bind(("B8:27:EB:C8:F5:28",port))
+serve_client()
 
-server_sock.listen(1)
+#server_sock.listen(1)
 
-client_sock,address = server_sock.accept()
+#client_sock,address = server_sock.accept()
 
-print("Accepted from ",address)
-while True:
-	data = client_sock.recv(1024)
-	print data
- 	action=data.split()
-	doAction(action[0],action[1]) 			
-	if data == "CLOSE 0":
-		break
+#print("Accepted from ",address)
+#while True:
+#	data = client_sock.recv(1024)
+#	print data
+# 	action=data.split()
+#	doAction(action[0],action[1]) 			
+#	if data == "CLOSE 0":
+#		break
 
 
-client_sock.close()
-server_sock.close()
+#client_sock.close()
+#server_sock.close()
 
 
