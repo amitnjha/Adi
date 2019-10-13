87469a3cade6138c47adf36fe680fc74d1da907b
Amit Jha <amitnjha@gmail.com>
2019-10-13 04:57:55 -0500

    make controller compatible with modern Pis

--- pyscripts/btcontroller.py
+++ pyscripts/btcontroller.py
@@ -5,17 +5,20 @@ from robotcontrol import *
 
 def serve_client():
         server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        port = 1
+        port = 0
         server_sock.bind(("",port))
-        server_sock.listen(1)
+        server_sock.listen(port)
+        uuid ="1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+        bluetooth.advertise_service(server_sock,"FooBar Service",uuid)
         client_sock,address = server_sock.accept()
         print("Accepted from ",address)
         while True:
                 try:
                         data = client_sock.recv(1024)
-                        print data
+                        #print(data)
                         action=data.split()
-                        doAction(action[0],action[1]) 			
+                        print(action)
+                        doAction(action[0].decode("utf-8"),int(action[1].decode("utf-8"))) 			
                         if data == "CLOSE 0":
                                 break
                 except:
@@ -23,7 +26,7 @@ def serve_client():
                         server_sock.close()
                         serve_client()
 
-print 'starting bt controller'
+print('starting bt controller')
 
 
 serve_client()
